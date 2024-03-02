18366dce2a63840ddf1390e7cacde7be399f8304
ibtihelbs <ibtihel.bensalah@outlook.fr>
2024-03-02 11:31:04 +0100

    with state

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const input = document.querySelector("#tast-user");
 const form = document.querySelector("form");
+const checkbox = document.querySelector("#checkbox");
 const tasks = [];
 const taskContainer = document.querySelector(".task-container");
 console.log(taskContainer);
@@ -7,6 +8,7 @@ function addTask(task) {
   tasks.push(task);
   console.log(tasks);
   input.value = "";
+  checkbox.checked = false;
 }
 function printTask() {
   for (let i = tasks.length - 1; i < tasks.length; i++) {
@@ -17,7 +19,12 @@ function printTask() {
     //create span
     const span = document.createElement("span");
     //add input value to span
-    span.innerHTML = tasks[i];
+    span.innerHTML = tasks[i].newTask;
+    // add bg-color if urgent
+    if (tasks[i].urgent) {
+      span.style.backgroundColor = "red";
+      span.style.color = "white";
+    }
     // create delete-btn
     const deleteBtn = document.createElement("button");
     deleteBtn.setAttribute("class", "delete-btn");
@@ -25,7 +32,6 @@ function printTask() {
     //insert single task
     singleTask.appendChild(span);
     singleTask.appendChild(deleteBtn);
-    console.log(singleTask);
     //append single task to taskContainer
     taskContainer.appendChild(singleTask);
   }
@@ -33,6 +39,8 @@ function printTask() {
 form.addEventListener("submit", function (e) {
   e.preventDefault();
   const newTask = input.value;
-  addTask(newTask);
+  const urgent = checkbox.checked;
+  console.log(urgent);
+  addTask({ newTask, urgent });
   printTask();
 });
